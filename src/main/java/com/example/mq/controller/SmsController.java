1e597fa1dc73be981e649497c6a7fdd3331e8e80
guanbinbin <binbin.guan@cloudchef.io>
2020-02-23 19:01:08 +0800

    修复smpp测试失败问题

--- src/main/java/com/example/mq/controller/SmsController.java
+++ src/main/java/com/example/mq/controller/SmsController.java
@@ -35,6 +35,7 @@ public class SmsController {
                          @RequestParam(name = "userName", required = false) String userName,
                          @RequestParam(name = "password", required = false) String password,
                          @RequestParam(name = "phonenumber", required = false) String phonenumber,
+                         @RequestParam(name = "sourceAddress", required = false) String sourceAddress,
                          @RequestParam(name = "verifyCode", required = false) String verifyCode
     ) {
         log.info("start to send sms notification, reciever,host {},port {}, userName {} password {} destinations is {} verifyCode {}", host, port, userName, password, phonenumber, verifyCode);
@@ -50,7 +51,7 @@ public class SmsController {
             BindResponse bind = session.bind(request);
             log.info("bind response debugString {},response command status {}",bind.debugString(),bind.getCommandStatus());
             String content = "[Registration]" + verifyCode + " is your verification code. Valid in 15 minutes. Please do not share this code with anyone else.";
-            SubmitSM submitSM = constructRequest(phonenumber, content);
+            SubmitSM submitSM = constructRequest(phonenumber,sourceAddress, content);
             //bund faild 会导致TCPIPConnection关闭从而导致outputStream关闭从而导致no
             SubmitSMResp response = session.submit(submitSM);
             log.info("send message result {},command status is {}",response.debugString(),response.getCommandStatus());
@@ -60,10 +61,11 @@ public class SmsController {
 
     }
 
-    private SubmitSM constructRequest(String phoneNumber,String content) throws WrongLengthOfStringException, UnsupportedEncodingException {
+    private SubmitSM constructRequest(String phoneNumber,String sourceAddress, String content) throws WrongLengthOfStringException, UnsupportedEncodingException {
         String recipientPhoneNumber = phoneNumber;
 
         SubmitSM request = new SubmitSM();
+        request.setSourceAddr(createAddress(sourceAddress));
         request.setDestAddr(createAddress(recipientPhoneNumber));
         request.setShortMessage(content,Data.ENC_UTF8);
         request.setReplaceIfPresentFlag((byte) 0);
@@ -79,9 +81,9 @@ public class SmsController {
     private Address createAddress(String address) throws WrongLengthOfStringException {
         Address addressInst = new Address();
         // national ton
-        addressInst.setTon((byte) 1);
+        addressInst.setTon((byte) 5);
         // numeric plan indicator
-        addressInst.setNpi((byte) 1);
+        addressInst.setNpi((byte) 0);
         addressInst.setAddress(address, Data.SM_ADDR_LEN);
         return addressInst;
     }

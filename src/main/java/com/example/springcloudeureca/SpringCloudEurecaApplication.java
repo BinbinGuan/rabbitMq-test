46f15373fb8743922e780efe51d40a3859c188a8
guanbinbin <binbin.guan@cloudchef.io>
2021-05-10 11:27:20 +0800

    提交

---
+++ src/main/java/com/example/springcloudeureca/SpringCloudEurecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudeureca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcloudeureca/SpringCloudEurecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudeureca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
